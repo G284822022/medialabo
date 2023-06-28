7e7db5655d1794802a2d9ba33b3f46f1e3b26b35
G284822022 <228482@st.takushoku-u.ac.jp>
2023-06-28 20:58:40 +0900

    kazuate

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -26,7 +26,7 @@ function hantei() {
   console.log(kaisu + '回目の予想:' + yoso);
 }
 if(kaisu < 3) {
-  if(yoso == kotae){
+  if(yoso === kotae){
     console.log('正解です。おめでとう！');
   }else if(yoso < kotae){
     console.log('まちがい。答えはもっと大きいですよ');
@@ -34,8 +34,8 @@ if(kaisu < 3) {
     console.log('まちがい。答えはもっと小さいですよ');
   }
 
-}else if(kaisu == 3){
-  if(yoso == kotae){
+}else if(kaisu === 3){
+  if(yoso === kotae){
     console.log('正解です。おめでとう！');
     kaisu = kaisu + 3;
   }else{
